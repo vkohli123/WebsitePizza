65d6c12ce8d194e975b0cb02e12d27b3142b0c15
vedant kohli <110954074+vkohli123@users.noreply.github.com>
2023-01-11 16:24:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Custom Script
+
+$(document).ready(function(){
+    // owl banner Slider
+    $("#banner-slider .owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        nav:true
+    });
+    
+    })
